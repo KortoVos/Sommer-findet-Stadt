76b52d2b3cdaf61c0154d110d151983ff0a9a515
KortoVos <65664998+LHofschlaeger@users.noreply.github.com>
2025-06-14 11:50:38 +0200

    test

---
+++ src/data/productList.ts
@@ -0,0 +1,52 @@
+const productList = {
+    title: 'My Storehouse',
+    pages: [
+        {
+            subTitle: 'Sofa Cake',
+            gradient: ['#ffe372', '#fc7700'],
+            items: [
+                { name: 'Sofa', dimensions: '100x100', image: './img/chair1.png' },
+                { name: 'Easychair & Table', dimensions: '150x100', image: './img/chair2.png' },
+                { name: 'Easychair Round', dimensions: '50x50', image: './img/chair3.png' },
+            ],
+        },
+        {
+            subTitle: 'Blob Sofa',
+            gradient: ['#b16ffc', '#724ec8'],
+            items: [
+                { name: 'Carrots', dimensions: '15x2', image: './img/chair4.png' },
+                { name: 'Grape', dimensions: '50x50', image: './img/sofa.png' },
+                { name: 'Pear', dimensions: '10x10', image: './img/chair1.png' },
+            ],
+        },
+        {
+            subTitle: 'Herb Pods',
+            gradient: ['#60f1d8', '#58dec8'],
+            items: [
+                { name: 'Mint', dimensions: '220x180', image: './img/chair1.png' },
+                { name: 'Lemon', dimensions: '40x40', image: './img/chair1.png' },
+                { name: 'Cherry', dimensions: '10x20', image: './img/chair1.png' },
+            ],
+        },
+        {
+            subTitle: 'Te Pods',
+            gradient: ['#e698ba', '#cea0b4'],
+            items: [
+                { name: 'Mint', dimensions: '220x180', image: './img/chair1.png' },
+                { name: 'Lemon', dimensions: '40x40', image: './img/chair1.png' },
+                { name: 'Cherry', dimensions: '10x20', image: './img/chair1.png' },
+            ],
+        },
+        {
+            subTitle: 'GEG Pods',
+            gradient: ['#722480', '#8b6392'],
+            items: [
+                { name: 'Mint', dimensions: '220x180', image: './img/chair1.png' },
+                { name: 'Lemon', dimensions: '40x40', image: './img/chair1.png' },
+                { name: 'Cherry', dimensions: '10x20', image: './img/chair1.png' },
+            ],
+        }
+    ],
+};
+
+export default productList;
